85d9bce5c6609400f1eab50cb1cca0b087c1d578
German Garcia Gonzalez <ggarciag.business@outlook.com>
2019-09-15 18:47:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(array_of_strings, event_name) {
+  let cards = []
+  for (let i = 0; i < array_of_strings.length; i++) {
+      cards.push(`Thank you, ${array_of_strings[i]}, for the wonderful ${event_name} gift!`);
+  }
+  return cards;
+}
+
+
+function countdown(num) {
+  while (num > 0) {
+    console.log(num);
+    num -= 1;
+  }
+  console.log(num);
+}
